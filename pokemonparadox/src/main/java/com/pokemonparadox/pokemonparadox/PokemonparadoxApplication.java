d84a2c59613e2508911e38e4b3f3557bb18fa7e5
ArKxdy <the.maniaxofficial@gmail.com>
2024-07-18 01:02:20 +0530

    build: downgraded to java 17

--- pokemonparadox/src/main/java/com/pokemonparadox/pokemonparadox/PokemonparadoxApplication.java
+++ pokemonparadox/src/main/java/com/pokemonparadox/pokemonparadox/PokemonparadoxApplication.java
@@ -8,6 +8,7 @@ public class PokemonparadoxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PokemonparadoxApplication.class, args);
+		System.out.println("Test");
 	}
 
 }

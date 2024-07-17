68ce2323a60c33ee4f52fface7b9322e2b0fb1c6
ArKxdy <the.maniaxofficial@gmail.com>
2024-07-18 00:48:20 +0530

    build: added springboot-java 21 application

---
+++ pokemonparadox/src/main/java/com/pokemonparadox/pokemonparadox/PokemonparadoxApplication.java
@@ -0,0 +1,13 @@
+package com.pokemonparadox.pokemonparadox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonparadoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonparadoxApplication.class, args);
+	}
+
+}
